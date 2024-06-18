94a47f890f7f39d825fd75581a294dc21659179c
nanasnumber <nanasnumber@gmail.com>
2024-06-18 14:35:03 -0400

    Update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://nanasnumber.github.io',
+  site: 'https://nanasnumber.com',
+  integrations: [sitemap()],
 })
